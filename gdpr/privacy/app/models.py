b860e140a537c134f09b41e4e065a99b1f0f9e5f
Rudresh Panchal <rudreshpanchal11@gmail.com>
2018-05-20 19:18:17 +0530

    initialized attribute action configuration model

--- gdpr/privacy/app/models.py
+++ gdpr/privacy/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+# Create your models here.
+
+
+class Attribute_Configuration(models.Model):
+    # Contains the primary name of the attribute.
+    # Will create a one to many mapping for aliases and action details
+    attribute_name = models.CharField(max_length=150)
+    ATTRIBUTE_ACTION_CHOICES = (
+        ('supp', 'supression'),
+        ('gen', 'generalization'),
+        ('del', 'deletion'),
+    )
+    attribute_action = models.CharField(
+        max_length=5, choices=ATTRIBUTE_ACTION_CHOICES)
