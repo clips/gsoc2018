0dd9497ac63fa99bda1ace81bc6a88931cb1b075
Rudresh Panchal <rudreshpanchal11@gmail.com>
2018-05-23 00:54:31 +0530

    Added supression config model

--- gdpr/privacy/app/admin.py
+++ gdpr/privacy/app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Attribute_Configuration
+from .models import Attribute_Configuration, Supression_Configuration
 # Register your models here.
 admin.site.register(Attribute_Configuration)
+admin.site.register(Supression_Configuration)
--- gdpr/privacy/app/models.py
+++ gdpr/privacy/app/models.py
@@ -16,3 +16,16 @@ class Attribute_Configuration(models.Model):
     )
     attribute_action = models.CharField(
         max_length=5, choices=ATTRIBUTE_ACTION_CHOICES)
+
+
+class Supression_Configuration(models.Model):
+    attribute = models.OneToOneField(Attribute_Configuration, primary_key=True)
+    # The field below gives number of characters to suppress. Can also use %
+    suppress_number = models.IntegerField()
+    # The field below specifies the percentage of characters to suppress
+    suppress_percent = models.FloatField()
+
+    def clean(self, *args, **kwargs):
+        if not self.suppress_number and not self.suppress_percent:
+            raise Exception(
+                'You need to mention either supress number of bits or percentage')
