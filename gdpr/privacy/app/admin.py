37992689f91202ed80b5c1967cec6ed20500f0b9
Rudresh Panchal <rudreshpanchal11@gmail.com>
2018-05-23 14:02:03 +0530

    added deletion configuration model

--- gdpr/privacy/app/admin.py
+++ gdpr/privacy/app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Attribute_Configuration, Supression_Configuration
+from .models import Attribute_Configuration, Supression_Configuration, Deletion_Configuration
 # Register your models here.
 admin.site.register(Attribute_Configuration)
 admin.site.register(Supression_Configuration)
+admin.site.register(Deletion_Configuration)
--- gdpr/privacy/app/models.py
+++ gdpr/privacy/app/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class Attribute_Configuration(models.Model):
     # Contains the primary name of the attribute.
     # Will create a one to many mapping for aliases and action details
-    attribute_name = models.CharField(max_length=150)
+    attribute_title = models.CharField(max_length=150)
     ATTRIBUTE_ACTION_CHOICES = (
         ('supp', 'supression'),
         ('gen', 'generalization'),
@@ -24,8 +24,13 @@ class Supression_Configuration(models.Model):
     suppress_number = models.IntegerField()
     # The field below specifies the percentage of characters to suppress
     suppress_percent = models.FloatField()
-
+    # Will check if atleast one of the two is provided, if no, will throw an error
     def clean(self, *args, **kwargs):
         if not self.suppress_number and not self.suppress_percent:
             raise Exception(
                 'You need to mention either supress number of bits or percentage')
+
+
+class Deletion_Configuration(models.Model):
+    attribute = models.OneToOneField(Attribute_Configuration, primary_key=True)
+    replacement_name = models.TextField()
