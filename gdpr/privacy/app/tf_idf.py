737f7fae75f1dcc678b1d8a0c7616462123bdf1c
Rudresh Panchal <rudreshpanchal11@gmail.com>
2018-06-22 04:43:18 +0530

    Added obtaining idf scores function

--- gdpr/privacy/app/tf_idf.py
+++ gdpr/privacy/app/tf_idf.py
@@ -4,6 +4,7 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import re
 import json
+from math import log
 stop_words = set(stopwords.words('english'))
 # set(stopwords.words('english'))
 
@@ -39,5 +40,24 @@ def generate_idf_counts(document_text):
         json.dump(idf_counts_dict, fp)
 
 
-generate_idf_counts(
-    "China has a strong economy that is growing at a rapid pace. However politically it differs greatly from the US Economy. Economy china china greatly US")
+def obtain_idf_scores(document_text):
+    tokens = word_tokenize(document_text)
+    regex_pattern_for_tokens = re.compile('[a-zA-Z]')
+    # List comprehension below removes non alphabet characters and stopwords
+    tokens = [token.lower() for token in tokens if token.lower()
+              not in stop_words and re.match(regex_pattern_for_tokens, token)]
+    # For the IDF counts only unique tokens required hence remove the rest
+    tokens = list(set(tokens))
+    idf_score_dict = {}
+    with open('tf_idf_scores/idf_counts.json', 'r') as fp:
+        idf_counts_dict = json.load(fp)
+    # The current IDF scoring scheme is inverse document frequency smoothened. Can be expanded
+    for token in tokens:
+        number_of_documents = idf_counts_dict['number_of_documents']
+        try:
+            score = log(1 + number_of_documents /
+                        (idf_counts_dict[token]))
+        except KeyError:
+            score = log(1 + number_of_documents)
+        idf_score_dict[token] = score
+    return idf_score_dict
