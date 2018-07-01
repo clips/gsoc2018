cf2aef12e263afc3682548aa77e3dfc132a3a4d4
Rudresh Panchal <rudreshpanchal11@gmail.com>
2018-07-02 03:55:29 +0530

    added user_id based file naming in tf_idf

--- gdpr/privacy/app/tf_idf.py
+++ gdpr/privacy/app/tf_idf.py
@@ -9,7 +9,13 @@ stop_words = set(stopwords.words('english'))
 # set(stopwords.words('english'))
 
 
-def generate_idf_counts(document_text):
+def generate_idf_counts(user_id, document_text):
+    ''' 
+    Saves the document and wordcount to the knowledgebase
+    '''
+    if not os.path.exists('tf_idf_scores'):
+        os.makedir('tf_idf_scores')
+
     tokens = word_tokenize(document_text)
     regex_pattern_for_tokens = re.compile('[a-zA-Z]')
     # List comprehension below removes non alphabet characters and stopwords
@@ -18,16 +24,16 @@ def generate_idf_counts(document_text):
     # For the IDF counts only unique tokens required hence remove the rest
     tokens = list(set(tokens))
     idf_counts_dict = {}
+    file_path = 'tf_idf_scores/' + str(user_id) + '_idf_counts.json'
     try:
         # Open the idf scores file and load the idf_counts_dict
-        with open('tf_idf_scores/idf_counts.json', 'r') as fp:
+        with open(file_path, 'r') as fp:
             idf_counts_dict = json.load(fp)
             idf_counts_dict['number_of_documents'] += 1
     except FileNotFoundError:
         # If the file does not exist, create it
         idf_counts_dict = {'number_of_documents': 1}
-        os.mkdir('tf_idf_scores')
-        with open('tf_idf_scores/idf_counts.json', 'w') as fp:
+        with open(file_path, 'w') as fp:
             json.dump(idf_counts_dict, fp)
     # Iterate through tokens and increase their count in the dict
     for token in tokens:
@@ -40,7 +46,7 @@ def generate_idf_counts(document_text):
         json.dump(idf_counts_dict, fp)
 
 
-def obtain_idf_scores(document_text):
+def obtain_idf_scores(user_id, document_text):
     tokens = word_tokenize(document_text)
     regex_pattern_for_tokens = re.compile('[a-zA-Z]')
     # List comprehension below removes non alphabet characters and stopwords
@@ -49,7 +55,8 @@ def obtain_idf_scores(document_text):
     # For the IDF counts only unique tokens required hence remove the rest
     tokens = list(set(tokens))
     idf_score_dict = {}
-    with open('tf_idf_scores/idf_counts.json', 'r') as fp:
+    file_path = 'tf_idf_scores/' + str(user_id) + '_idf_counts.json'
+    with open(file_path, 'r') as fp:
         idf_counts_dict = json.load(fp)
     # The current IDF scoring scheme is inverse document frequency smoothened.
     # Can be expanded
@@ -76,8 +83,11 @@ def obtain_tf_scores(document_text):
     return token_counts
 
 
-def obtain_tf_idf_scores(document_text):
-    idf_scores_dict = obtain_idf_scores(document_text)
+def obtain_tf_idf_scores(user_id, document_text):
+    '''
+    Obtains the tf_idf scores tokenwise as a dictionary
+    '''
+    idf_scores_dict = obtain_idf_scores(user_id, document_text)
     tf_scores_dict = obtain_tf_scores(document_text)
     size_of_document = tf_scores_dict['size_of_document']
     tf_idf_scores_dict = {}
